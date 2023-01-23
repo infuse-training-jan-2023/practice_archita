67f558f904b575d4d0be8c49d30c9930f588ba0c
archita19 <madhusmitasamudra@gmail.com>
2023-01-23 06:15:46 +0530

    multiply() commit removed: soft reset

--- calculator.rb
+++ calculator.rb
@@ -15,8 +15,14 @@ class Calculator
         diff = num1 - num2
         puts "Difference between #{num1} and #{num2} is #{diff}"
     end
+
+    def multiply()
+        prod = num1 * num2
+        puts "Product of #{num1} and #{num2} is #{prod}"
+    end
 end
 
 c = Calculator.new(45.56, -10)
 c.add()
 c.subtract()
+c.multiply()
