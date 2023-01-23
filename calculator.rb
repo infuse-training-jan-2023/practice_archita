d935547590dd2e92cbe5c08ecbf60c7fb647ab79
archita19 <madhusmitasamudra@gmail.com>
2023-01-23 06:12:28 +0530

    subtract() added

--- calculator.rb
+++ calculator.rb
@@ -10,7 +10,13 @@ class Calculator
         sum = num1 + num2
         puts "Sum of #{num1} and #{num2} is #{sum}"
     end
+
+    def subtract()
+        diff = num1 - num2
+        puts "Difference between #{num1} and #{num2} is #{diff}"
+    end
 end
 
 c = Calculator.new(45.56, -10)
 c.add()
+c.subtract()
