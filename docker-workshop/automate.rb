094dcab03526a23a51e982415853358f6cb6d255
archita19 <madhumsmitasamudra@gmail.com>
2023-02-05 13:11:35 +0530

    changes added as mentioned in PR

--- docker-workshop/automate.rb
+++ docker-workshop/automate.rb
@@ -1,16 +1,16 @@
-require_relative 'testFramework'
+require_relative 'test_framework'
 
 class AutomateHeadphonezone
-    attr_accessor :obj, :wait
+    attr_accessor :tf_obj, :wait
 
     def initialize
         # @wait = Selenium::WebDriver::Wait.new(:timeout => 20)
-        @obj = Test_Framework.new
-        @obj.navigate_to_url("https://www.headphonezone.in/")
+        @tf_obj = Test_Framework.new
+        @tf_obj.navigate_to_url("https://www.headphonezone.in/")
     end
 
     def get_page_title
-        puts "Page Title is #{obj.get_page_title}"
+        puts "Page Title is #{tf_obj.get_page_title}"
     end
 
     # def login
@@ -49,31 +49,31 @@ class AutomateHeadphonezone
     def search(search_txt)
         # sleep(2)
         # begin
-        #     popup = obj.find_element(css: "#lion-loyalty-panel-custom-css div:nth-of-type(5) div a")
-        #     if obj.is_displayed(popup)
-        #         obj.click_element(popup)
+        #     popup = tf_obj.find_element(css: "#lion-loyalty-panel-custom-css div:nth-of-type(5) div a")
+        #     if tf_obj.is_displayed(popup)
+        #         tf_obj.click_element(popup)
         #     end
         # rescue => e
         #     puts e.message
         # end
-        popup = obj.find_element(css: "#lion-loyalty-panel-custom-css div:nth-of-type(5) div a")
-        obj.click_element(popup)
+        popup = tf_obj.find_element(css: "#lion-loyalty-panel-custom-css div:nth-of-type(5) div a")
+        tf_obj.click_element(popup)
         sleep(2)
 
-        search_icon = obj.find_element(css: "ul.header__linklist:nth-child(2) > li:nth-child(1) > a:nth-child(1)")
+        search_icon = tf_obj.find_element(css: "ul.header__linklist:nth-child(2) > li:nth-child(1) > a:nth-child(1)")
         # wait.until{search_icon}
-        obj.click_element(search_icon)
+        tf_obj.click_element(search_icon)
         sleep(2)
 
-        search_input = obj.find_element(css: ".predictive-search__input")
+        search_input = tf_obj.find_element(css: ".predictive-search__input")
         # wait.until{search_input}
-        obj.send_keys(search_input, search_txt)
+        tf_obj.send_keys(search_input, search_txt)
         sleep(2)
 
-        found = obj.find_element(css: "#search-drawer > footer > button")        
-        if obj.get_text(found) == "VIEW ALL RESULTS"
+        found = tf_obj.find_element(css: "#search-drawer > footer > button")        
+        if tf_obj.get_text(found) == "VIEW ALL RESULTS"
             puts "Search successfull for #{search_txt}"
-            obj.click_element(found)
+            tf_obj.click_element(found)
         else
             puts "#{search_txt} not found"
         end
@@ -81,63 +81,63 @@ class AutomateHeadphonezone
     end
 
     def apply_filters
-        brand = obj.find_element(css: "#facet-filters-form > div > div:nth-child(1) > button")
-        obj.click_element(brand)
-        obj.send_keys(brand, :arrow_down)
-        obj.send_keys(brand, :arrow_down)
-        obj.send_keys(brand, :arrow_down)
-        obj.send_keys(brand, :arrow_down)
-        brand_opt1 = obj.find_element(xpath: '//*[@id="filter.p.vendor-1"]')
-        obj.click_element(brand_opt1)        
-        brand_opt2 = obj.find_element(xpath: '//*[@id="filter.p.vendor-4"]')
-        obj.click_element(brand_opt2)
-        obj.click_element(brand)
+        brand = tf_obj.find_element(css: "#facet-filters-form > div > div:nth-child(1) > button")
+        tf_obj.click_element(brand)
+        tf_obj.send_keys(brand, :arrow_down)
+        tf_obj.send_keys(brand, :arrow_down)
+        tf_obj.send_keys(brand, :arrow_down)
+        tf_obj.send_keys(brand, :arrow_down)
+        brand_opt1 = tf_obj.find_element(xpath: '//*[@id="filter.p.vendor-1"]')
+        tf_obj.click_element(brand_opt1)        
+        brand_opt2 = tf_obj.find_element(xpath: '//*[@id="filter.p.vendor-4"]')
+        tf_obj.click_element(brand_opt2)
+        tf_obj.click_element(brand)
         puts "Filter applied"
         # sleep(2)
 
-        item = obj.find_element(css: "#facet-main > product-list > div > product-item:nth-child(4)")
-        obj.click_element(item)
+        item = tf_obj.find_element(css: "#facet-main > product-list > div > product-item:nth-child(4)")
+        tf_obj.click_element(item)
         sleep(3)
     end
 
     def add_to_cart
-        add_to_cart_btn = obj.find_element(css: "#AddToCart > span.loader-button__text")
-        obj.click_element(add_to_cart_btn)
+        add_to_cart_btn = tf_obj.find_element(css: "#AddToCart > span.loader-button__text")
+        tf_obj.click_element(add_to_cart_btn)
         sleep(3)
 
-        add_to_cart_success = obj.find_element(xpath: "//*[@id='shopify-section-header']/cart-notification/div/div/div")
-        if obj.is_displayed(add_to_cart_btn)
+        add_to_cart_success = tf_obj.find_element(xpath: "//*[@id='shopify-section-header']/cart-notification/div/div/div")
+        if tf_obj.is_displayed(add_to_cart_btn)
             puts "Added to cart"
         else
             puts "Not added to cart"
         end
 
-        add_to_cart_icon = obj.find_element(css: "#shopify-section-header > store-header > div > div > div > ul > li:nth-child(3) > a")
-        obj.click_element(add_to_cart_icon)
+        add_to_cart_icon = tf_obj.find_element(css: "#shopify-section-header > store-header > div > div > div > ul > li:nth-child(3) > a")
+        tf_obj.click_element(add_to_cart_icon)
         sleep(2)        
         
-        close_btn = obj.find_element(xpath: "//*[@id='mini-cart']/header/button")
-        obj.click_element(close_btn)
+        close_btn = tf_obj.find_element(xpath: "//*[@id='mini-cart']/header/button")
+        tf_obj.click_element(close_btn)
         sleep(3)
     end
 
     def go_to_help_center
-        help = obj.find_element(css: "#shopify-section-header > store-header > div > div > nav > desktop-navigation > ul > li:nth-child(7) > a")
-        obj.click_element(help)
+        help = tf_obj.find_element(css: "#shopify-section-header > store-header > div > div > nav > desktop-navigation > ul > li:nth-child(7) > a")
+        tf_obj.click_element(help)
         puts "Go to help center"
         sleep(2)
-        help_search = obj.find_element(id: "support-search-input")
-        obj.send_keys(help_search, "orders")
-        obj.send_keys(help_search, :enter)
+        help_search = tf_obj.find_element(id: "support-search-input")
+        tf_obj.send_keys(help_search, "orders")
+        tf_obj.send_keys(help_search, :enter)
         sleep(2)
-        solution = obj.find_element(xpath: "/html/body/div[1]/div[2]/section/ul[2]/li[1]/a")
-        obj.click_element(solution)
+        solution = tf_obj.find_element(xpath: "/html/body/div[1]/div[2]/section/ul[2]/li[1]/a")
+        tf_obj.click_element(solution)
         sleep(2)
     end
 
     def go_to_home_page
-        logo = obj.find_element(css: "body > div.page > header > div > div > a > span > img")
-        obj.click_element(logo)
+        logo = tf_obj.find_element(css: "body > div.page > header > div > div > a > span > img")
+        tf_obj.click_element(logo)
         puts "Go to home page"
         sleep(2)
     end
@@ -152,7 +152,7 @@ class AutomateHeadphonezone
     end
 end
 
-obj = AutomateHeadphonezone.new
-obj.automate
+tf_obj = AutomateHeadphonezone.new
+tf_obj.automate
 
 
--- docker-workshop/test_framework.rb
+++ docker-workshop/test_framework.rb
@@ -1,35 +1,19 @@
 require 'selenium-webdriver'
+require_relative 'driver'
 
 class Test_Framework
     attr_accessor :driver
 
     def initialize
-        # Selenium::WebDriver::Chrome.driver_path = "C:\\Users\\Archita\\Documents\\Work_Infuse\\Training\\selenium\\chromedriver_win32\\chromedriver.exe"
-        # @driver = Selenium::WebDriver.for :chrome
-        # @driver.manage.window.maximize
-        Selenium::WebDriver::Chrome.driver_path = "/opt/chromedriver-109.0.5414.74/chromedriver"
-        options = Selenium::WebDriver::Chrome::Options.new
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--window-size=1920,1080")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--disable-dev-shm-usage")
-        @driver = Selenium::WebDriver.for :chrome, options: options
+        @driver = Driver.new.driver_method()
     end
 
     def navigate_to_url(url)
-        # uri = URI.parse(url)
-        # if (uri.is_a? (URI::HTTPS) or uri.is_a? (URI::HTTP))
-        #     puts("Openning #{url} in browser")
-        #     driver.get(url)
-        # else
-        #     puts("#{url} is an invalid url")
-        # end
         begin
             driver.get(url)
             return "Navigated to url successfully"
-        rescue => e
-            raise e
+        rescue 
+            return "Invalid url" 
         end
             
     end
@@ -37,8 +21,8 @@ class Test_Framework
     def get_page_title
         begin
             return driver.title
-        rescue => e
-            raise e
+        rescue 
+            return "Invalid url" 
         end
     end
 
@@ -47,8 +31,8 @@ class Test_Framework
 
         begin
             return driver.find_element(key => value)
-        rescue => e
-            raise e
+        rescue
+            return "Invalid identifier or value" 
         end
     end
 
@@ -58,15 +42,15 @@ class Test_Framework
         begin
             return driver.find_elements(key => value)
         rescue => e
-            raise e
+            return "Invalid identifier or value" 
         end
     end
 
     def get_text(element)
         begin
             return element.text
-        rescue => e
-            raise e
+        rescue 
+            return "Element not found" 
         end
     end
 
@@ -74,8 +58,8 @@ class Test_Framework
         begin
             element.click
             return "Clicked successfully"
-        rescue => e
-            raise e
+        rescue 
+            return "Element not found" 
         end
     end
 
@@ -83,24 +67,24 @@ class Test_Framework
         begin
             element.send_keys(key)
             return "#{key}"
-        rescue => e
-            raise e
+        rescue 
+            return "Element not found or key is invalid" 
         end
     end
 
     def get_attribute(element, attribute_val)
         begin
             return element.attribute(attribute_val)
-        rescue => e
-            raise e
+        rescue 
+            return "Element not found or attribute is invalid" 
         end
     end
 
     def is_displayed(element)
         begin
             return element.displayed?
-        rescue => e
-            raise e
+        rescue 
+            return "Element not found" 
         end
     end
 end
