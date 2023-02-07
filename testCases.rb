7f4e466ab461f62d71b2489f7d89d390cc841aae
archita19 <madhumsmitasamudra@gmail.com>
2023-02-07 15:58:04 +0530

    added 2 more methods and tests

--- testCases.rb
+++ testCases.rb
@@ -51,50 +51,50 @@ class TestBook < Test::Unit::TestCase
 
     def test_array_functions_valid_case_noninclusive_range()
         arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        assg2 = PrintArrayElements2.new()
-        assert_equal([1, 2, 3, 4, 0], assg2.non_inclusive_range(arr, 2, 7))
+        obj = PrintArrayElements.new()
+        assert_equal([1, 2, 3, 4, 0], obj.non_inclusive_range(arr, 2, 7))
     end
 
     def test_array_functions_empty_arr_noninclusive_range()
         arr = []
-        assg2 = PrintArrayElements2.new()
-        assert_equal(-1, assg2.non_inclusive_range(arr, 2, 5))
+        obj = PrintArrayElements.new()
+        assert_equal(-1, obj.non_inclusive_range(arr, 2, 5))
     end
 
     def test_array_functions_invalid_start_index_noninclusive_range()
         arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        assg2 = PrintArrayElements2.new()
-        assert_equal(-1, assg2.non_inclusive_range(arr, -1, 5))
+        obj = PrintArrayElements.new()
+        assert_equal(-1, obj.non_inclusive_range(arr, -1, 5))
     end
 
     def test_array_functions_invalid_end_index_noninclusive_range()
         arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        assg2 = PrintArrayElements2.new()
-        assert_equal(-1, assg2.non_inclusive_range(arr, 2, 9))
+        obj = PrintArrayElements.new()
+        assert_equal(-1, obj.non_inclusive_range(arr, 2, 9))
     end
 
     def test_array_functions_valid_case_start_len()
         arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        assg2 = PrintArrayElements2.new()
-        assert_equal([1, 2, 3, 4, 0, -1], assg2.start_and_length(arr, 2, 6))
+        obj = PrintArrayElements.new()
+        assert_equal([1, 2, 3, 4, 0, -1], obj.start_and_length(arr, 2, 6))
     end
 
     def test_array_functions_empty_arr_start_len()
         arr = []
-        assg2 = PrintArrayElements2.new()
-        assert_equal(-1, assg2.start_and_length(arr, 2, 5))
+        obj = PrintArrayElements.new()
+        assert_equal(-1, obj.start_and_length(arr, 2, 5))
     end
 
     def test_array_functions_invalid_start_index()
         arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        assg2 = PrintArrayElements2.new()
-        assert_equal(-1, assg2.start_and_length(arr, -9, 4))
+        obj = PrintArrayElements.new()
+        assert_equal(-1, obj.start_and_length(arr, -9, 4))
     end
 
     def test_array_functions_invalid_len()
         arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        assg2 = PrintArrayElements2.new()
-        assert_equal(-1, assg2.start_and_length(arr, 2, 8))
+        obj = PrintArrayElements.new()
+        assert_equal(-1, obj.start_and_length(arr, 2, 8))
     end
 end
 
